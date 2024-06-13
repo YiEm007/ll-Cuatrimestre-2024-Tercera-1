9eb6ee468ca36e435316c5a720b74b762d3b43da
Yostin <yostinsmmart@gmail.comm>
2024-06-12 21:44:51 -0600

    Primera Clase

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php require_once "./partesuperior.php"?>
+
+
+<?php require_once "./parteinferior.php"?>
---
+++ partesuperior.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="English">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Proyecto de Tercera 1" content="Página de Prueba">
+    <title>
+
+        Tercera 1
+
+    </title>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+<menu>
+    <div class="menu-izquierda">
+        <img src="./images/minion.png" alt="">
+
+    </div>
+</menu>
+
+
+
